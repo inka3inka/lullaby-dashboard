26871195b7fad289ca42704773e9d066bd9ecf6c
inka3inka <inka3inka@gmail.com>
2020-05-21 16:51:11 +0200

    basic elements html and styles

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 import '../styles/main.scss';
+
+window.addEventListener("DOMContentLoaded", () => {
+
+//Elements
+  const clock = document.querySelector('.clock');
+
+
+  setInterval(() => {
+    const data = new Date();
+    clock.innerText = data.getHours() + ":" + data.getMinutes();
+    console.log(data.getHours() + ":" + data.getMinutes())
+  }, 1000)
+
+})
+
+
