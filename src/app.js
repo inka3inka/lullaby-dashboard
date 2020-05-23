a8a905e99df30e56a705c4c886a48f3d07ee9277
inka3inka <inka3inka@gmail.com>
2020-05-23 21:55:02 +0200

    To do list basics elements

--- src/app.js
+++ src/app.js
@@ -4,13 +4,27 @@ window.addEventListener("DOMContentLoaded", () => {
 
 //Elements
   const clock = document.querySelector('.clock');
+  const addTaskButton = document.querySelector('.button__add-task');
+  const list = document.querySelector('.list-elements');
 
 
   setInterval(() => {
     const data = new Date();
     clock.innerText = data.getHours() + ":" + data.getMinutes();
     console.log(data.getHours() + ":" + data.getMinutes())
-  }, 1000)
+  }, 1000);
+
+  function addListElement() {
+    const newElement = document.createElement("li");
+    list.appendChild(newElement).innerText = "Hello!";
+  }
+
+  addTaskButton.addEventListener('click', addListElement)
+
+
+
+
+
 
 })
 
