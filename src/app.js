25bfd43e44596a5c46188ded2faccc833b54a9f0
inka3inka <inka3inka@gmail.com>
2020-05-24 20:43:11 +0200

    ToDo list elements and checkbox - handle function

--- src/app.js
+++ src/app.js
@@ -6,17 +6,22 @@ window.addEventListener("DOMContentLoaded", () => {
   const clock = document.querySelector('.clock');
   const addTaskButton = document.querySelector('.button__add-task');
   const list = document.querySelector('.list-elements');
+  const listInput = document.querySelector('.list-input');
 
 
   setInterval(() => {
     const data = new Date();
-    clock.innerText = data.getHours() + ":" + data.getMinutes();
-    console.log(data.getHours() + ":" + data.getMinutes())
+    clock.innerText =
+      (data.getHours() < 10 ? "0" + data.getHours() : data.getHours()) + ":" +
+      (data.getMinutes() < 10 ? "0" + data.getMinutes() : data.getMinutes());
   }, 1000);
 
   function addListElement() {
     const newElement = document.createElement("li");
-    list.appendChild(newElement).innerText = "Hello!";
+    list.appendChild(newElement);
+    newElement.classList.add("list-element");
+    newElement.innerHTML = `${listInput.value}<input type="checkbox" class="checkbox">`;
+    listInput.value = ""
   }
 
   addTaskButton.addEventListener('click', addListElement)
