33b777ab6391202487845117304f7edc6e84c68d
inka3inka <inka3inka@gmail.com>
2020-05-25 02:15:25 +0200

    Input values validation

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ window.addEventListener("DOMContentLoaded", () => {
   const list = document.querySelector('.list-elements');
   const listInput = document.querySelector('.list-input');
 
-
+//Set time
   setInterval(() => {
     const data = new Date();
     clock.innerText =
@@ -16,18 +16,40 @@ window.addEventListener("DOMContentLoaded", () => {
       (data.getMinutes() < 10 ? "0" + data.getMinutes() : data.getMinutes());
   }, 1000);
 
+//List handler
+
   function addListElement() {
     const newElement = document.createElement("li");
     list.appendChild(newElement);
     newElement.classList.add("list-element");
-    newElement.innerHTML = `${listInput.value}<input type="checkbox" class="checkbox">`;
-    // listInput.value = ""
+    newElement.innerHTML = `${listInput.value}
+                            <div class="list-element__edit">
+                              <input type="checkbox" class="checkbox">
+                              <i class="far fa-edit"></i>
+                              <i class="far fa-trash-alt"></i>
+                            </div>`;
+    listInput.value = "";
+    addTaskButton.setAttribute("disabled", "true")
   }
 
-  addTaskButton.addEventListener('click', addListElement)
+  //Add task by click button
+  addTaskButton.addEventListener('click', addListElement);
 
-
-
+  //Key press effects
+  listInput.addEventListener('keyup', function(event) {
+    //Add task by press Enter key
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      addTaskButton.click();
+    }
+    //Validate input value
+    else if (listInput.value.trim() != "") {
+      addTaskButton.removeAttribute("disabled")
+    }
+    else {
+      addTaskButton.setAttribute("disabled", "true")
+    }
+  })
 
 
 
