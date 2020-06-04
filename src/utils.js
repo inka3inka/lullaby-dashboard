97ff5606dfede6196ea3fec92daa47f345324d2d
inka3inka <inka3inka@gmail.com>
2020-06-04 22:49:50 +0200

    1 millestone styled, list elements styled

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 const clock = document.querySelector('.clock');
+const listContainer = document.querySelector('.list');
 const list = document.querySelector('.list-elements');
 const video = document.querySelector('.video');
 const listName = document.querySelector('.list-name');
@@ -13,13 +14,18 @@ export function loadVideo(data) {
 // Load to do list
 export function loadList(data) {
 
+  //List background
+  listContainer.style.backgroundColor = data.savedLists[0].backgroundListColor;
+
   //List label
   listName.innerText = data.savedLists[0].label;
+  listName.style.color = data.savedLists[0].fontListColor;
 
   //List elements
   data.savedLists[0].toDos.map(element => {
     const newElement = document.createElement("li");
     list.appendChild(newElement);
+    newElement.style.color = element.color;
     newElement.classList.add("list-element");
     newElement.innerHTML =
       `${element.name}
